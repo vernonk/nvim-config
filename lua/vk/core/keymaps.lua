0de2611041f41a36b0557bdb85b35f936e45bd99
Vernon Kesner <vernon.kesner@bottomline.com>
2024-08-29 15:12:06 -0400

    Add ESC to clear search highlights

--- lua/vk/core/keymaps.lua
+++ lua/vk/core/keymaps.lua
@@ -27,3 +27,6 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
+-- clear search highlight with escape
+keymap.set("n", "<ESC>", ":noh<CR>", { desc = "Clear search highlights" })
+
