b81e87778ab6bdd6f06bcebdea4ec12aaf001771
Vernon Kesner <vernon.kesner@bottomline.com>
2024-09-23 09:17:10 -0400

    Update linting so eslint_d does autofix on save

--- lua/vk/plugins/linting.lua
+++ lua/vk/plugins/linting.lua
@@ -4,21 +4,37 @@ return {
   config = function()
     local lint = require("lint")
 
+    local eslint_d = require("lint").linters.eslint_d
+    eslint_d.args = {
+      '--stdin',
+      '--stdin-filename',
+      '%filepath',
+    }
+
     lint.linters_by_ft = {
       javascript = { "eslint_d" },
       typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      python = { "pylint" },
     }
 
+    -- Run lint
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
+      end,
+    })
+
+    -- Run eslint_d --fix on save to autofix issues
+    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+      pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
+      callback = function()
+        vim.cmd([[
+          silent! !eslint_d --fix % 
+          edit! 
+        ]])
       end,
     })
 
